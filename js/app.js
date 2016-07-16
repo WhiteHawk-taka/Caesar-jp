f2fdb8783eb51c1da84ad056ab00f7f247067177
whitehawk <white-hawk-taka@outlook.com>
2016-07-16 14:16:58 +0900

    add ID

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(event) {
+  var normal = document.getElementById('normal');
+  var encrypt = document.getElementById('encrypt');
+  var keys = document.getElementById('keys');
+  var decrypt = document.getElementById('decrypt');
+  var cipher = document.getElementById('cipher');
   //ここにコードを書いていく
 
 }, false);
